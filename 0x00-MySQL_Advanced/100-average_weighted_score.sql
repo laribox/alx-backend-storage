629b58bbb4b7e8cc927e64d38e928ed31714d465
laribox <laribox2014@gmail.com>
2024-10-16 17:37:42 -0700

    task 11 answer

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -13,7 +13,7 @@ BEGIN
         INNER JOIN projects
         ON projects.id = corrections.project_id
         WHERE corrections.user_id = user_id)
-    WHERE users.id = user_id;
+    WHEREusers.users.id = user_id;
 END
 //
 
