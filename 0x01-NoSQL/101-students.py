18b2ea7afb0bb26882101dfb88d1f35d2df4625e
laribox <laribox2014@gmail.com>
2024-10-25 15:24:13 -0700

    task 12

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+'''This module contains the function `top_students`.
+'''
+
+
+def top_students(mongo_collection):
+    '''Prints all students in a collection sorted by average score.
+    '''
+    students = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
+                    },
+                    'topics': 1,
+                },
+            },
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
+    return students#!/usr/bin/env python3
+'''This module contains the function `top_students`.
+'''
+
+
+def top_students(mongo_collection):
+    '''Prints all students in a collection sorted by average score.
+    '''
+    students = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
+                    },
+                    'topics': 1,
+                },
+            },
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
+    return students
