56b8861e4c2614c5cd5a033aa0b96dc2f161e4ab
laribox <laribox2014@gmail.com>
2024-10-25 15:10:54 -0700

    task 10

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
-"""This module contains the function `insert_school`
+"""This module contains the function `update_topics`
 """
 
 
 def update_topics(mongo_collection, name, topics):
-    """changes all topics of a school document based on the name
-    """
+    """Updates the topics of a given document"""
     if not isinstance(name, str) or not isinstance(topics, list):
         raise TypeError("name must be a string, and topics must be a list")
 
     if not all(isinstance(t, str) for t in topics):
         raise TypeError("topics must be a list of strings")
 
-    mongo_collection.update_many({"name":name},{"$set":{"topics",topics}})
+    try:
+        mongo_collection.update_many(
+            {"name": name},
+            {"$set": {"topics": topics}},
+        )
+    except Exception as e:
+        print("failed to update many documents: {}".format(str(e)))
