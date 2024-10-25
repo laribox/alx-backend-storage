f8ac9d7edab89741effd01359fdf06f0d6a049a0
laribox <laribox2014@gmail.com>
2024-10-25 15:07:08 -0700

    task 10

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -6,4 +6,10 @@
 def update_topics(mongo_collection, name, topics):
     """changes all topics of a school document based on the name
     """
+    if not isinstance(name, str) or not isinstance(topics, list):
+        raise TypeError("name must be a string, and topics must be a list")
+
+    if not all(isinstance(t, str) for t in topics):
+        raise TypeError("topics must be a list of strings")
+
     mongo_collection.update_many({"name":name},{"$set":{"topics",topics}});
