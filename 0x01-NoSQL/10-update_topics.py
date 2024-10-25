8fe62fe86516a4d02fd2b24e6fd1888aa965b91c
laribox <laribox2014@gmail.com>
2024-10-25 15:05:24 -0700

    task 10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""This module contains the function `insert_school`
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """changes all topics of a school document based on the name
+    """
+    mongo_collection.update_many({"name":name},{"$set":{"topics",topics}});
